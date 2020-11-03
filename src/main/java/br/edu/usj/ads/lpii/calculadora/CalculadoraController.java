da0fedd45e0d67667a64ea3cbb607800134f6b42
Matheus Alves <matttalves@gmail.com>
2020-11-03 20:18:16 -0300

    add historico de resultados

--- src/main/java/br/edu/usj/ads/lpii/calculadora/CalculadoraController.java
+++ src/main/java/br/edu/usj/ads/lpii/calculadora/CalculadoraController.java
@@ -1,5 +1,8 @@
 package br.edu.usj.ads.lpii.calculadora;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +10,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CalculadoraController {
+
+    List<String> historico = new ArrayList<>();
 
     @PostMapping(value = "/somar")
     public ModelAndView postSomar(@RequestParam String a, @RequestParam String operacao, @RequestParam String b) {
@@ -16,6 +21,11 @@ public class CalculadoraController {
         Operacao op = new Operacao();
         
         modelAndView.addObject("resultado", op.calcular(a, operacao, b));
+        
+        historico.add(op.getValorFinal().toString());
+
+        modelAndView.addObject("historicos", historico);
+
 
         return modelAndView;
     }
